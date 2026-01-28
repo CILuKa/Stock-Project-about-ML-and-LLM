90228def77d16c49afcb7f51d6eeb5fb3ee0d22c
lby <308651190@QQ.COM>
2026-01-28 21:48:52 +0800

    news_part and lgb model

---
+++ news_factory/news_collector.py
@@ -0,0 +1,16 @@
+"""
+this script is a tool to collect news about stock and the relate policies about stock market and nation.
+主要难点：
+1.高效可实用的搜索引擎api
+
+2.规范搜索格式
+
+3.巨大规模搜索结果的处理及存储（按照日期的）
+
+4.合适的网站或许可以解决一些搜索压力
+
+注意事项：
+1.规范输出
+
+2.每日的或每一段时间的自动更新
+"""
